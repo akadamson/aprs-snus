b470a6da0e8ed5abbe9fcab764413e708cfbe983
Alan Adamson <aadamson@highrf.com>
2014-06-09 17:41:57 -0400

    added copyright/license to the top of the file

--- aprs-snus.pl
+++ aprs-snus.pl
@@ -2,6 +2,22 @@
 # note, you'll still need to set the permission to 755 if you want
 # to run this from the command line
 # you should be able to pipe the output to a file and run this in the background
+#
+#    Copyright (C) 2014  AK Adamson - W7QO
+#==========================================================================
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#==========================================================================
 
 use warnings;
 use strict;
